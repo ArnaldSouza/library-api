59bd122ba8728c2f8d62aeabff68817ad29526a7
Arnald Souza <arnald.souza472@gmail.com>
2026-09-11 21:03:41 -0300

    chore: add test dependencies and integration test base

---
+++ src/test/java/com/arnaldsouza/libraryapi/AbstractIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.arnaldsouza.libraryapi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public abstract class AbstractIntegrationTest {
+
+    static final PostgreSQLContainer<?> POSTGRES =
+            new PostgreSQLContainer<>("postgres:17-alpine");
+
+    static {
+        POSTGRES.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+}
--- src/test/java/com/arnaldsouza/libraryapi/LibraryApiApplicationTests.java
+++ src/test/java/com/arnaldsouza/libraryapi/LibraryApiApplicationTests.java
@@ -1,13 +1,10 @@
 package com.arnaldsouza.libraryapi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LibraryApiApplicationTests {
+class LibraryApiApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {
     }
-
 }
